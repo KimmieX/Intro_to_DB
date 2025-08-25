b6450e7c3c12c2e1f0a3a8faaa491c7369453864
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-08-25 23:16:28 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,12 @@
--- Use the database passed as an argument
-USE alx_book_store;
-
--- Query to get the full description of the Books table
 SELECT 
-    COLUMN_NAME AS 'Column Name',
-    DATA_TYPE AS 'Data Type',
-    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
-    IS_NULLABLE AS 'Is Nullable',
-    COLUMN_DEFAULT AS 'Default Value'
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA,
+    COLUMN_KEY
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = 'alx_book_store';
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
